073e500646021de1de33df577cf6b1deec4feb7b
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2023-01-23 23:36:55 +0700

    start create Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] CreateStringArray()
+{
+    string[] newStringArr = new string[3];
+    int count = newStringArr.Length;
+    int i = 0;
+
+    do
+    {
+        Console.WriteLine("Input three string in turn: ");
+        newStringArr[i] = Console.ReadLine();
+        i++; count--;
+    }
+    while (count > 0);
+
+    return newStringArr;
+}
+
+void ShowStringArr(string[] array)
+{
+    int count = array.Length;
+    int i = 0;
+
+    while(count > 0)
+    {
+        Console.Write($"{array[i]}");
+        Console.Write(" ");
+        i++; count--;
+    }
+    
+}
+
+string[] strArr = CreateStringArray();
+ShowStringArr(strArr);
+
+
+
