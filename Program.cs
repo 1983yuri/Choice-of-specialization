40c09a6ffa91af73031122ca5428c52b67d5cdd6
Архипов Юрий Владимирович <arkhipov_yuri@mail.ru>
2023-01-24 09:50:51 +0700

    pre checking program in Program.cs file

--- Program.cs
+++ Program.cs
@@ -6,13 +6,34 @@
 
     do
     {
-        Console.WriteLine("Input three string in turn: ");
-        newStringArr[i] = Console.ReadLine();
-        i++; count--;
+        Console.WriteLine($"Input the {i + 1} of three string in turn: ");
+        newStringArr[i] = Console.ReadLine(); // Можно сразу проверять как на null так и не записывать в массив значения превышающие 3 знака Нужно реализовать! Хотя, просят новый массив!
+        i++; count--;                         // Надо подумать, как организовать проверку на количество символов. В одной из лекций было такое, где текст заменяли
+        
     }
     while (count > 0);
 
     return newStringArr;
+}
+
+string[] TransformStringArray(string[] array)
+{
+    
+    string[] newarray = new string[size];
+
+    for( int i = 0, j = 0; i < newarray.Length; i++)
+    {
+        string str = newarray[i];
+        int length = str.Length; 
+        if(length <= 3)
+        {
+            newarray[j] = array[i];
+            j++;
+        }
+        else i++; 
+    }
+    return array;
+    
 }
 
 void ShowStringArr(string[] array)
@@ -31,6 +52,8 @@ void ShowStringArr(string[] array)
 
 string[] strArr = CreateStringArray();
 ShowStringArr(strArr);
+string[] newStrArr = TransformStringArray(strArr);
+ShowStringArr(newStrArr);
 
 
 
